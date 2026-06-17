11a893fcca0f12cdd314e53dc56bdc51472b3e4f
RottenSky <52703071+skygamer913@users.noreply.github.com>
2026-06-17 19:41:08 -0300

    Update main.c
    Adicionando um menu no main.c

--- main.c
+++ main.c
@@ -1,6 +1,49 @@
 #include <stdio.h>
 
+
 int main(){
-    
+    int opcao = -1;
+
+    while(opcao != 0){
+
+        printf("\n---- Menu ----\n");
+        printf("1. Listar conteúdo do disco\n");
+        printf("2. Listar conteúdo de um arquivo\n");
+        printf("3. Exibir os atributos de um arquivo\n");
+        printf("4. Renomear um arquivo\n");
+        printf("5. Inserir/criar um arquivo\n");
+        printf("6. Apagar/remover um arquivo\n");
+        printf("0. Sair\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+        printf("\n-----------------\n");
+
+        switch(opcao){
+            case 1:
+                // Listar conteúdo do disco
+                break;
+            case 2:
+                // Listar conteúdo de um arquivo
+                break;
+            case 3:
+                // Exibir os atributos de um arquivo
+                break;
+            case 4:
+                // Renomear um arquivo
+                break;
+            case 5:
+                // Inserir/criar um arquivo
+                break;
+            case 6:
+                // Apagar/remover um arquivo
+                break;
+            case 0:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opção inválida. Tente novamente.\n");
+        }
+    }
+
     return 0;
 }
